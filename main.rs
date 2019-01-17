2e145aadffd503c2f41bae8e8a7148e8a0826c37
Yoshihiro Kameda <kameda.sbng@gmail.com>
2019-01-17 20:50:53 +0900

    implement FizzBuzz :)

--- main.rs
+++ main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    for x in 1..101 {
+
+        if x % 15 == 0 {
+            println!("fizzbuzz");
+        } else if x % 3 == 0 {
+            println!("fizz");
+        } else if x % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", x);
+        }
+    }
 }
+
